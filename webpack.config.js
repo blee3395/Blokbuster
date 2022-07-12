f9112123f94faa9d2f6a37a2c6e8ae5e77f5b155
Brennan <blee3395@gmail.com>
2022-07-12 16:56:33 -0400

    changed webpack module exclude path

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@ module.exports = {
     rules: [
       { 
         test: /\.jsx?/, 
-        exclude: /(node_modules | bower_components)/,
+        // exclude: /(node_modules)/,
+        exclude: path.resolve(__dirname, './node_modules'),
         use: {
           loader: 'babel-loader',
           options: {
