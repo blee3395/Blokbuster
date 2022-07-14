c1ba69cd7b5b543e45d3e63121ad9a035bbd25ff
Brennan <blee3395@gmail.com>
2022-07-14 01:41:21 -0400

    Able to retrieve favs and load to Home

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Slider from './Slider.jsx'
+import FavSlider from './FavSlider.jsx';
 
 const Home = (props) => {
 
@@ -10,6 +11,12 @@ const Home = (props) => {
         type = {'poster'} 
         qty = {5}
       />
+      <h2>My Favorites</h2>
+      <FavSlider 
+        qty = {10}
+      />
+
+
     </div>
   )
 
